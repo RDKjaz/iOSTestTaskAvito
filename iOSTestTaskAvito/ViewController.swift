9d83b4a8169307add0acceaa1aa95db93d72cee6
Radik Gazetdinov <RDKJaz@gmail.com>
2023-08-28 19:16:42 +0300

    Initial Commit

---
+++ iOSTestTaskAvito/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  iOSTestTaskAvito
+//
+//  Created by Radik Gazetdinov on 28.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+}
